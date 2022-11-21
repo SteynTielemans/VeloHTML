946fef2b21649e429bd20cd4147a5845a80fbd3f
SteynTielemans <s131282@ap.be>
2022-11-21 12:22:25 +0100

    kan data naar json file duwen

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import sqlalchemy as sa
 from urllib.request import urlopen
 import json
 
-class json():
+class data():
     def __init__():
         pass
     def getdata():
         url = "https://www.velo-antwerpen.be/availability_map/getJsonObject"
         response = urlopen(url)
         data_json = json.loads(response.read())
+        with open("data.json","w") as f:
+            json.dump(data_json,f)
+
+data.getdata()
