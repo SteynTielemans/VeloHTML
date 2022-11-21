6e1e009f9067a9e03038d2a0e0f77e84e74d7201
SteynTielemans <s131282@ap.be>
2022-11-21 11:52:45 +0100

    json halen uit url en printen

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import sqlalchemy as sa
+
+# import urllib library
+from urllib.request import urlopen
+
+# import json
+import json
+# store the URL in url as
+# parameter for urlopen
+url = "https://www.velo-antwerpen.be/availability_map/getJsonObject"
+
+# store the response of URL
+response = urlopen(url)
+
+# storing the JSON response
+# from url in data
+data_json = json.loads(response.read())
+
+# print the json response
+print(data_json)
