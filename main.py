64a80b789ad3fb43374b15954ae6d65effad841d
SteynTielemans <s131282@ap.be>
2022-11-27 15:23:27 +0100

    added encoding

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlalchemy as sa
 from urllib.request import urlopen
 import json
+ENCODING = "utf-8"
 
 class data():
     def __init__(self):
@@ -9,10 +9,10 @@ class data():
         url = "https://www.velo-antwerpen.be/availability_map/getJsonObject"
         response = urlopen(url)
         data_json = json.loads(response.read())
-        with open("data.json","w") as f:
+        with open("data.json","w",encoding = ENCODING) as f:
             json.dump(data_json,f)
     def printdata(self):
-        with open("data.json","r") as f:
+        with open("data.json","r",encoding = ENCODING) as f:
             print(f)
 
-data.printdata
+data().printdata()
