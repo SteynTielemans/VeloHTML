13b27ff1cc9dc248905b87517b96b504e80e4718
SteynTielemans <s131282@ap.be>
2022-11-27 15:29:15 +0100

    put data class into data.py

---
+++ data.py
@@ -0,0 +1,16 @@
+ENCODING = "utf-8"
+import json
+from urllib.request import urlopen
+
+class data():
+    def __init__(self):
+        pass
+    def getdata(self):
+        url = "https://www.velo-antwerpen.be/availability_map/getJsonObject"
+        response = urlopen(url)
+        data_json = json.loads(response.read())
+        with open("data.json","w",encoding = ENCODING) as f:
+            json.dump(data_json,f)
+    def printdata(self):
+        with open("data.json","r",encoding = ENCODING) as f:
+            print(f.read())
--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-from urllib.request import urlopen
-import json
-ENCODING = "utf-8"
-
-class data():
-    def __init__(self):
-        pass
-    def getdata(self):
-        url = "https://www.velo-antwerpen.be/availability_map/getJsonObject"
-        response = urlopen(url)
-        data_json = json.loads(response.read())
-        with open("data.json","w",encoding = ENCODING) as f:
-            json.dump(data_json,f)
-    def printdata(self):
-        with open("data.json","r",encoding = ENCODING) as f:
-            print(f.read())
+from data import data
 
 data().printdata()
