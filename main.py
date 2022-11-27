945c658628d4fc42335efc1cfb56930a769cdc81
SteynTielemans <s131282@ap.be>
2022-11-27 15:07:56 +0100

    added self to methods

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from urllib.request import urlopen
 import json
 
 class data():
-    def __init__():
+    def __init__(self):
         pass
-    def getdata():
+    def getdata(self):
         url = "https://www.velo-antwerpen.be/availability_map/getJsonObject"
         response = urlopen(url)
         data_json = json.loads(response.read())
         with open("data.json","w") as f:
             json.dump(data_json,f)
-    def printdata():
+    def printdata(self):
         with open("data.json","r") as f:
             print(f)
 
