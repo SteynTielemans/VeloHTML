986d6f5bf82ce4f4735ec163f0bfef907d3bf8aa
SteynTielemans <s131282@ap.be>
2022-11-27 23:53:53 +0100

    try to push data to db

--- sqlite.py
+++ sqlite.py
@@ -4,13 +4,14 @@ from data import data
 conn = sqlite3.connect('velo.db')
 
 c=conn.cursor()
-adress = []
+address = []
 bikes = []
 id = []
 lat = []
 lon = []
 name =[]
 slots =[]
+stationType = []
 status =[]
 
 c.execute("""CREATE TABLE IF NOT EXISTS velo (
@@ -21,23 +22,26 @@ c.execute("""CREATE TABLE IF NOT EXISTS velo (
             lon real,
             name text,
             slots integer,
+            stationType text,
             status text
 )""")
 
 file = data().opendata()
-# for value in file:
-#     adress.append(value['address'])
-#     bikes.append(value['bikes'])
-#     id.append(value['id'])
-#     lat.append(value['lat'])
-#     lon.append(value['lon'])
-#     name.append(value['name'])
-#     slots.append(value['slots'])
-#     status.append(value['status'])
+for value in file:
+    address.append(value['address'])
+    bikes.append(value['bikes'])
+    id.append(value['id'])
+    lat.append(value['lat'])
+    lon.append(value['lon'])
+    name.append(value['name'])
+    slots.append(value['slots'])
+    stationType.append(value['stationType'])
+    status.append(value['status'])
 
-# for adress1 in adress:
-#     print(adress)
-print(file)
+for number in range(len(id)):
+    c.execute("INSERT INTO velo VALUES (:address, :bikes, :id, :lat, :lon, :name, :slots, :stationType, :status)",
+    {'address':address[number],'bikes' : bikes[number],'id' : id[number],'lat' : lat[number],'lon' : lon[number],'name' : name[number],'slots' : slots[number],'stationType' : stationType[number],'status' : status[number]})
+    conn.commit()
 
 conn.commit()
 
