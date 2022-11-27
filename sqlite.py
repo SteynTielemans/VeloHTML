09c991761567632d455af4dc773e106dfe5e636e
SteynTielemans <s131282@ap.be>
2022-11-27 16:20:46 +0100

    add colums to db

--- sqlite.py
+++ sqlite.py
@@ -4,7 +4,16 @@ conn = sqlite3.connect('velo.db')
 
 c=conn.cursor()
 
-
+c.execute("""CREATE TABLE velo (
+            adress text,
+            bikes integer,
+            id integer,
+            lat real,
+            lon real,
+            name text,
+            slots integer,
+            status text
+            )""")
 
 conn.commit()
 
